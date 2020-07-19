f8f9b629f424afbaa2c8963b7203d802ab6d1ebc
Vo Van Trung <trungvv0511@gmail.com>
2020-07-19 22:53:41 +0700

    test for translate

--- sites.js
+++ sites.js
@@ -18,17 +18,7 @@ module.exports={
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36'
-                },        // request headers. format is the identical to that accepted by the Headers constructor (see below)
-        // body: null,         // request body. can be null, a string, a Buffer, a Blob, or a Node.js Readable stream
-        // redirect: 'follow', // set to `manual` to extract redirect headers, `error` to reject redirect
-        // signal: null,       // pass an instance of AbortSignal to optionally abort requests
-    
-        // // The following properties are node-fetch extensions
-        // follow: 20,         // maximum redirect count. 0 to not follow redirect
-        // timeout: 0,         // req/res timeout in ms, it resets on redirect. 0 to disable (OS limit applies). Signal is recommended instead.
-        // compress: true,     // support gzip/deflate content encoding. false to disable
-        // size: 0,            // maximum response body size in bytes. 0 to disable
-        // agent: null  
+                },        
         },
     fields: {
         title: {
